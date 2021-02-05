99905d1f55f9e20a89a8527b1a6bdaee892437ff
Dharmik Shah <dharm1k987@gmail.com>
2021-02-05 00:48:31 +0000

    forgot to add d.sh

---
+++ aws/d.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+#course="investing-in-stocks-the-complete-course-11-hour"
+#course="mastering-data-structures-algorithms-using-c-andc"
+course="the-ultimate-mysql-bootcamp-go-from-sql-beginner-to-expert"
+domain="https://www.learningcrux.com"
+main="$domain/course/$course"
+play="$domain/play/$course"
+
+sections=$(curl -s "$main")
+readarray -t lines <<< "$(echo $sections | grep -oP "\K(Section \d+:.*?)(?=<\/h2)")"
+#lines=("${lines[@]:5}")
+printf -- "%s\n" "${lines[@]}"
+len=${#lines[@]}
+len=2
+read -p "Continue? (y/n): " cont
+if [ "$cont" != "y" ]; then
+	exit
+fi
+
+for (( i=0; i<${len}; i++ ));
+
+do
+	dir="${lines[$i]}"
+	dir=$(echo $dir | sed 's/:/-/g')
+	printf "\ndir is $dir"
+	mkdir -p "$dir"
+
+	for j in {0..100}
+	do
+		url="$play/$i/$j/720"
+		final=$(curl -w "%{url_effective}" -I -L -s -S "$url" -o /dev/null)
+		gdrive=$(echo $final | grep -oP "\*\/\K(.+)(?=\?e=download)")
+		if [ "$url" = "$final" ]; then
+			printf "\nSkip onto next iteration\n---------------------\n"
+			break
+		fi
+
+		if [[ ! -z "$gdrive" ]]
+		then
+			printf "\nSection $i , Part $j"
+			/usr/bin/gdrive.sh "$gdrive" "$dir" &
+		else
+			basename=$(basename -- "$final")
+			escaped=$(echo $basename | sed 's@+@ @g;s@%@\\x@g' | xargs -0 printf "%b")
+			printf "\nDownloading $escaped ..."
+			curl -s -L "$final" -o "$dir/$escaped" &
+		fi
+		printf "\n"
+
+	done
+	printf "waiting..."
+	wait
+done
