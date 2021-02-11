54932d211f28524914fff7af6c350771f6bd5d8c
Dharmik Shah <dharm1k987@gmail.com>
2021-02-11 20:49:01 +0000

    add ddclient

--- aws/script.sh
+++ aws/script.sh
@@ -1,12 +1,12 @@
 home="/home/ubuntu"
-files=("$home/.bashrc" "$home/.htaccess" "$home/.profile" "$home/.pythoncwd.py" "/mnt/hdd/a.py" "/mnt/hdd/d.sh" "/etc/apache2/apache2.conf" "/usr/bin/gdrive.sh" "$home/.ssh/authorized_keys")
+files=("$home/.bashrc" "$home/.htaccess" "$home/.profile" "$home/.pythoncwd.py" "/mnt/hdd/a.py" "/mnt/hdd/d.sh" "/etc/apache2/apache2.conf" "/usr/bin/gdrive.sh" "$home/.ssh/authorized_keys" "/etc/default/ddclient" "/etc/ddclient.conf")
 for file in ${files[@]};
 do
 	if [[ -d "$file" ]]; then
 		continue
 	fi
 	touch $(basename $file)
-	cat "$file" > $(basename $file)
+	sudo cat "$file" > $(basename $file)
 
 done
 
